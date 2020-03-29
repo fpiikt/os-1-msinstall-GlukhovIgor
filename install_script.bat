60f09b4a4fe7ae59516c532d9cb8dd886029db11
Igor Glukhov <gluhov2000@gmail.com>
2020-03-29 17:40:01 +0300

    installs script + installation logs + jre config file

---
+++ install_script.bat
@@ -0,0 +1,7 @@
+7z1900-x64.msi /passive /norestart /le "D:\Study\OS\7zip-log.txt" INSTALLDIR="C:\Program Files\7-Zip"
+paintnet_4.2.10.exe /auto DESKTOPSHORTCUT=1 TARGETDIR="C:\Program Files\Graphics\Paint"
+inkscape-0.92.4-x64.msi /qr /norestart /lw "D:\Study\OS\inkscape-log.txt" INSTALLDIR="C:\Program Files\Graphics\Inkscape"
+LibreOffice_6.4.2_Win_x64.msi /passive /norestart /le "D:\Study\OS\libreoffice-install-log.txt"
+LibreOffice_6.4.2_Win_x64_helppack_ru.msi /passive /forcerestart
+npp.7.5.6.Installer.x64.exe /S
+jre-8u221-windows-x64.exe INSTALLCFG="D:\Study\OS\jre8_cfg.txt" /L*V "D:\Study\OS\jre-log.txt"
